18bd4503339d43a81b4b5251110cafc61633e0af
Andrew McOlash <amcolash@gmail.com>
2015-12-05 13:17:19 -0600

    Added in twitter functionality - commented out for testing purposes for now

--- water.py
+++ water.py
@@ -5,6 +5,8 @@ import RPi.GPIO as GPIO # Allows us to call our GPIO pins and names it just GPIO
 import time
 import signal
 import sys
+import auth
+import twitter
 
 GPIO.setmode(GPIO.BCM)  # Set's GPIO pins to BCM GPIO numbering
 INPUT_PIN = 23          # Sets our input pin, in this case 23
@@ -13,7 +15,12 @@ GPIO.setup(INPUT_PIN, GPIO.IN)  # Set our input pin to be an input
 OUTPUT_PIN = 24         # Set the hygrometer to a GPIO (keep it off most of the time)
 GPIO.setup(24, GPIO.OUT)
 
-lastMessage = 0
+twitter_api = twitter.Api(consumer_key=auth.consumer_key,
+                  consumer_secret=auth.consumer_secret,
+                  access_token_key=auth.access_token_key,
+                  access_token_secret=auth.access_token_secret)
+
+lastMessage = 0        # last time (in secs since epoch) that a notification was sent
 
 # Gracefully quit on Ctrl-C and reset GPIO
 def signal_handler(signal, frame):
@@ -26,10 +33,11 @@ signal.signal(signal.SIGINT, signal_handler)
 # Create a function to run when the input is high
 def inputLow(channel):
   global lastMessage
+  global twitter_api
 #  if (time.time() - lastMessage > 86400):
   if (time.time() - lastMessage > 3):
     lastMessage = time.time()
-    print 'Last message: ' + time.ctime(lastMessage)
+    #twitter_api.PostUpdate("Looks like I am pretty thirsty right now @amcolash should feed me soon :)")
 
 # Wait for the input to go high (no water), run the function when it does
 GPIO.add_event_detect(INPUT_PIN, GPIO.RISING, callback=inputLow, bouncetime=200)
@@ -41,5 +49,5 @@ while True:
   sleep(5);
 
   # Turn off voltage pin for 60 minutes to prevent wear
-  GPIO.output(24, False)
-  sleep(3600)
+  # GPIO.output(24, False)
+  # sleep(3600)
