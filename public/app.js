40cc17bfac09360a2fa3abc0e78c3b2a8f7cf9a8
Andrew McOlash <amcolash@gmail.com>
2015-12-14 23:24:38 -0600

    Better handling of dates / missing data

--- public/app.js
+++ public/app.js
@@ -4,6 +4,9 @@ $(document).ready(function() {
         renderTo: 'chart',
         type: 'spline',
       },
+      legend: {
+        enabled: false
+      },
       plotOptions: {
         spline: {
           marker: {
@@ -11,10 +14,24 @@ $(document).ready(function() {
           }
         }
       },
+      series: [{}],
       title: {
         text: 'Recent Water Values'
       },
+      tooltip: {
+        xDateFormat: '%A, %B %d, %I:%M%p'
+      },
       xAxis: {
+        dateTimeLabelFormats: {
+        	millisecond: '%I:%M:%S.%L',
+        	second: '%I:%M:%S',
+        	minute: '%I:%M%p',
+        	hour: '%I:%M%p',
+        	day: '%e. %b',
+        	week: '%e. %b',
+        	month: '%b \'%y',
+        	year: '%Y'
+        },
         type: 'datetime',
         title: {
           text: 'Date'
@@ -32,11 +49,7 @@ $(document).ready(function() {
         title: {
           text: null
         }
-      },
-      legend: {
-        enabled: false
-      },
-      series: [{}]
+      }
   };
 
   Highcharts.setOptions({
@@ -71,8 +84,17 @@ $(document).ready(function() {
     });
 
     $('#current').text(lastStatus ? 'Wet' : 'Dry');
-    $('#lastWet').text(lastWet);
-    $('#lastDry').text(lastDry);
+    if (lastWet != null && lastWet != undefined) {
+      $('#lastWet').text(new Date(lastWet).toLocaleString());
+    } else {
+      $('#lastWet').text('Not enough data');
+    }
+
+    if (lastDry != null && lastDry != undefined) {
+      $('#lastDry').text(new Date(lastDry).toLocaleString());
+    } else {
+      $('#lastDry').text('Not enough data');
+    }
 
     options.series[0].data = series;
     options.series[0].color = '#449944';
